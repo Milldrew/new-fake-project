4d09200779cb0aeb9fc3541b3be051b3e71b4d32
Andrew Miller <Andrew.patrick.miller.2021@gmail.com>
2025-04-07 08:31:16 -0600

    📅  | Mon Apr  7 08:31:16 MDT 2025

--- src/format.js
+++ src/format.js
@@ -1,8 +1,8 @@
-const {execSync} = require('child_process');
-pwd
+const { execSync } = require('child_process');
+pwd;
 try {
   console.log('Formatting code with Prettier...');
-  execSync('npx prettier --write "src/**/*.js"', {stdio: 'inherit'});
+  execSync('npx prettier --write "src/**/*.js"', { stdio: 'inherit' });
   console.log('Code formatted successfully.');
 } catch (error) {
   console.error('Error occurred while formatting code:', error);
